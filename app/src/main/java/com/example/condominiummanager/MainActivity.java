68175c6ebfb86852202423b6b3782e33c18e907a
I.A.Gzyk <ivangzyk@hotmail.com>
2020-03-13 21:19:55 -0300

    classe botoes

--- app/src/main/java/com/example/condominiummanager/MainActivity.java
+++ app/src/main/java/com/example/condominiummanager/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.condominiummanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import android.widget.ImageButton;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageButton site;
+    ImageButton entrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        site = (ImageButton) findViewById(R.id.site);
+        entrar = (ImageButton) findViewById(R.id.entrar);
+
+        site.setOnClickListener(new ImageButton.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                String endereco = "http://www.cmanager.com.br";
+
+                Uri uri = Uri.parse(endereco);
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+
+                startActivity(intent);
+            }
+        });
+
     }
 }
