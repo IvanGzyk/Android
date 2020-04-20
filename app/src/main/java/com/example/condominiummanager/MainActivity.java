bf6a9302fdf96b41b577b60f7e5f6d9bffa428a7
I.A.Gzyk <ivangzyk@hotmail.com>
2020-04-20 19:10:41 -0300

    Menu

--- app/src/main/java/com/example/condominiummanager/MainActivity.java
+++ app/src/main/java/com/example/condominiummanager/MainActivity.java
@@ -1,21 +1,25 @@
 package com.example.condominiummanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-    Button site;
-    Button entrar;
+   /* Button site;
+    Button entrar;*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+/*
         site = (Button) findViewById(R.id.site);
         entrar = (Button) findViewById(R.id.entrar);
 
@@ -28,11 +32,37 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
             }
-        });
+        });*/
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.entrar:
+                Intent tela = new Intent(this, MainLoginActivity.class);
+                startActivity(tela);
+                return true;
+            case R.id.site:
+                String endereco = "http://www.cmanager.com.br";
+                Uri uri = Uri.parse(endereco);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+                return true;
+            default:
+        }
+        return super.onOptionsItemSelected(item);
+    }
+/*
     public void TeleDeLogin(View v){
         Intent tela = new Intent(this, MainLoginActivity.class);
         startActivity(tela);
-    }
+    }*/
 }
