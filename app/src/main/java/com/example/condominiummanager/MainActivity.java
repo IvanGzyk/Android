2a3dfb5fdefcac910abe1a1eccc53da2c127ee2f
I.A.Gzyk <ivangzyk@hotmail.com>
2020-06-19 16:38:11 -0300

    mensagem na tela

--- app/src/main/java/com/example/condominiummanager/MainActivity.java
+++ app/src/main/java/com/example/condominiummanager/MainActivity.java
@@ -13,27 +13,10 @@ import android.widget.Button;
 import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-   /* Button site;
-    Button entrar;*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-/*
-        site = (Button) findViewById(R.id.site);
-        entrar = (Button) findViewById(R.id.entrar);
-
-        site.setOnClickListener(new Button.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-
-                String endereco = "http://www.cmanager.com.br";
-                Uri uri = Uri.parse(endereco);
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
-            }
-        });*/
-
     }
 
     @Override
@@ -60,9 +43,4 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-/*
-    public void TeleDeLogin(View v){
-        Intent tela = new Intent(this, MainLoginActivity.class);
-        startActivity(tela);
-    }*/
 }
--- app/src/main/java/com/example/condominiummanager/MainLoginActivity.java
+++ app/src/main/java/com/example/condominiummanager/MainLoginActivity.java
@@ -6,9 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainLoginActivity extends AppCompatActivity {
     Button entrar;
+    EditText login;
+    EditText senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +20,11 @@ public class MainLoginActivity extends AppCompatActivity {
         setContentView(R.layout.login);
 
     entrar = (Button) findViewById(R.id.entrar);
+    login = (EditText) findViewById(R.id.cpfCnpj);
+    senha = (EditText) findViewById(R.id.senha);
 }
     public void ProximaPagina(View v){
+        Toast.makeText(MainLoginActivity.this, "Bem vindo ao Cmanager!!!", Toast.LENGTH_LONG).show();
         Intent tela = new Intent(this, MainPrincipalActivity.class);
         startActivity(tela);
     }
