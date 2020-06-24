58c1d3abfcd6d06bfd19fbbafd2195aa87486e73
I.A.Gzyk <ivangzyk@hotmail.com>
2020-06-24 10:05:56 -0300

    Classes Edital e Reclamações

---
+++ app/src/main/java/com/example/condominiummanager/Edital.java
@@ -0,0 +1,31 @@
+package com.example.condominiummanager;
+
+class Edital {
+    private String aviso;
+    private String data_p;
+    private String date_r;
+
+    public String getAviso() {
+        return aviso;
+    }
+
+    public void setAviso(String aviso) {
+        this.aviso = aviso;
+    }
+
+    public String getData_p() {
+        return data_p;
+    }
+
+    public void setData_p(String data_p) {
+        this.data_p = data_p;
+    }
+
+    public String getDate_r() {
+        return date_r;
+    }
+
+    public void setDate_r(String date_r) {
+        this.date_r = date_r;
+    }
+}
---
+++ app/src/main/java/com/example/condominiummanager/Reclamacoes.java
@@ -0,0 +1,22 @@
+package com.example.condominiummanager;
+
+class Reclamacoes {
+    private String usuario;
+    private String reclamacao;
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getReclamacao() {
+        return reclamacao;
+    }
+
+    public void setReclamacao(String reclamacao) {
+        this.reclamacao = reclamacao;
+    }
+}
